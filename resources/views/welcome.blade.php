89b8c537ebbdecde5afbad8c95cf2bf7c32db117
gprodrigues95 <gustavopiresrodrigues@gmail.com>
2021-02-26 22:14:06 -0300

    Ocultando comentários importantes na view "welcome".

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'HDC Events')
 
-@section('content') <!-- Palavras com @ antes são "diretivas do Blade" -->
+@section('content') {{-- Palavras com @ antes são "diretivas do Blade" --}}
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
@@ -14,12 +14,12 @@
     <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
-        @foreach($events as $event) <!-- Quando uma variável precisa ser usada no na diretiva do Blade, NÃO é necessário as duas chaves {{}} -->
+        @foreach($events as $event) {{-- Quando uma variável precisa ser usada no na diretiva do Blade, NÃO é necessário as duas chaves {{}} --}}
         <div class="card col-md-3">
             <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
                 <p class="card-date">10/09/2020</p>
-                <h5 class="card-title">{{ $event->title }}</h5> <!-- Quando uma variável precisa ser usada no meio do HTML, é necessário as duas chaves {{}} -->
+                <h5 class="card-title">{{ $event->title }}</h5> {{-- Quando uma variável precisa ser usada no meio do HTML, é necessário as duas chaves {{}} --}}
                 <p class="card-participantes">X Participantes</p>
                 <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
