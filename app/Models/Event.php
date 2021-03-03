19c01563d58962a571742522ca6e6b0b81a4b5b4
gprodrigues95 <gustavopiresrodrigues@gmail.com>
2021-03-03 15:26:53 -0300

    Adição de comentário.

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//Model é responsável pela interação entre as requisições do banco
+//Basicamente é a entidade que vai fazer a conexão entre o banco e a aplicação por meio da ORM Eloquent 
+
 class Event extends Model
 {
     use HasFactory;
