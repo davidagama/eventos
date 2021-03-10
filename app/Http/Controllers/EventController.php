f93372127713ebb25e822e36db3c650d923f8bc2
gprodrigues95 <gustavopiresrodrigues@gmail.com>
2021-03-09 21:05:22 -0300

    Fim do projeto. Fim das modificações.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -84,9 +84,25 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+
+            //Este trecho vai percorrer cada um dos eventos para verificar de quais eventos do BD o usuário logado está participando
+            //Não é a melhor opção para quem deseja desempenho na aplicação
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard() {
@@ -95,7 +111,11 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', 
+            ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]
+        );
     }
 
     public function destroy($id) {
@@ -109,7 +129,14 @@ class EventController extends Controller
     //Nesta function, basicamente resgatamos dados do banco e enviamos para a view chamada edit para os dados serem atualizados
     public function edit($id) {
 
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        //Este if impede que um usuário que não seja dono de um evento faça edições no mesmo.
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
 
@@ -136,6 +163,30 @@ class EventController extends Controller
         Event::findOrFail($request->id)->update($data);
         
         return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+
+    }
+
+    public function joinEvent($id) {
+        
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no ' . $event->title);
+
+    }
+
+    public function leaveEvent($id) {
+        
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do ' . $event->title);
 
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,8 @@ class User extends Authenticatable
         return $this->hasMany('App\Models\Event');
     }
 
+    public function eventsAsParticipant() {
+        return $this->belongsToMany('App\Models\Event');
+    }
+
 }
